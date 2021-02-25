d2a0738bab393cfaaaf5421b98174366f84707af
Artur Lizakowski <arturlizakowski@gmail.com>
2021-02-25 23:49:37 +0100

    commit 3

---
+++ API/Controllers/PostsController.cs
@@ -0,0 +1,13 @@
+using API.Data;
+
+namespace API.Controllers
+{
+    public class PostsController
+    {
+        private readonly DataContext _context;
+    public PostsController(DataContext context)
+    {
+      _context = context;
+    }
+    }
+}
