50fdfb2e05c45b97a67e0bd38aac27c9db30bbd3
Jakub Kalamon <jakub.kalamon@gmail.com>
2021-02-26 02:04:38 +0100

    login form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<nb-layout [center]="false" [withScroll]="false" [windowMode]="false">
+  template: `<nb-layout [center]="true">
     <nb-layout-column [start]="true" ><router-outlet></router-outlet></nb-layout-column>
     
   </nb-layout>`
