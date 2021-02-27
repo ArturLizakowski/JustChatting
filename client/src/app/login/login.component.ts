afdbd32334160d50166b87a14f925a3ae55ca55a
Jakub Kalamon <jakub.kalamon@gmail.com>
2021-02-27 01:38:38 +0100

    fx

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UserLogin } from 'src/models/user-login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public userLogin: UserLogin = {
+    userName: '',
+    password: ''
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
